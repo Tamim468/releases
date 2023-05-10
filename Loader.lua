2219096ca678a7361738bec581260851e7dace58
Tamim468 <79998020+Tamim468@users.noreply.github.com>
2023-05-10 15:37:02 +0300

    Update and rename faypo-pub.lua to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,17 @@
+repeat wait() until game:IsLoaded()
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+-- Actual Checks
+local games = {
+    8542259458,
+    855499080,
+    662417684
+}
+for i, v in pairs(games) do
+    if i == game.PlaceId or i == game.GameId then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Tamim468/releases/main/games/' .. tostring(game.PlaceId)))()
+        else
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Tamim468/releases/main/Universal.lua'))()
+    end
+end
